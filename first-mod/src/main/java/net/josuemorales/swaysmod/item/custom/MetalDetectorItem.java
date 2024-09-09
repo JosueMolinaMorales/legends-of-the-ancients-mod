fbe9f565175921b79409e9b90fb163b9b2384957
Josue Morales <molinajosue92@hotmail.com>
2024-09-08 21:53:50 -0400

    add custom tag

--- first-mod/src/main/java/net/josuemorales/swaysmod/item/custom/MetalDetectorItem.java
+++ first-mod/src/main/java/net/josuemorales/swaysmod/item/custom/MetalDetectorItem.java
@@ -1,5 +1,6 @@
 package net.josuemorales.swaysmod.item.custom;
 
+import net.josuemorales.swaysmod.utils.ModTags;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
@@ -7,10 +8,14 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Properties properties) {
@@ -18,7 +23,7 @@ public class MetalDetectorItem extends Item {
     }
 
     @Override
-    public InteractionResult useOn(UseOnContext pContext) {
+    public @NotNull InteractionResult useOn(UseOnContext pContext) {
         if (pContext.getLevel().isClientSide()) {
             return InteractionResult.SUCCESS;
         }
@@ -51,11 +56,16 @@ public class MetalDetectorItem extends Item {
         return InteractionResult.SUCCESS;
     }
 
+    @Override
+    public void appendHoverText(ItemStack pStack, TooltipContext pContext, List<Component> pTooltipComponents, TooltipFlag pTooltipFlag) {
+        pTooltipComponents.add(Component.translatable("tooltip.sways_mod.metal_detector.tooltip"));
+    }
+
     private void outputValuableCoordinates(BlockPos below, Player player, Block block) {
         player.sendSystemMessage(Component.literal("Found " + I18n.get(block.getDescriptionId()) + " at " + "(" + below.getX() + ", " + below.getY() + ", " + below.getZ() + ")"));
     }
 
     private boolean isValuableBlock(BlockState state) {
-        return state.is(Blocks.IRON_ORE) || state.is(Blocks.GOLD_ORE) || state.is(Blocks.DIAMOND_ORE);
+        return state.is(ModTags.Blocks.METAL_DETECTOR_VALUABLES);
     }
 }
